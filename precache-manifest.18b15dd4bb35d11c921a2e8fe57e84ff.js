46bc83e21eb79aa4491a34263ba15785c21df0f7
natasha17 <17061990@inbox.ru>
2020-05-03 12:47:57 +0300

    Updates

---
+++ precache-manifest.18b15dd4bb35d11c921a2e8fe57e84ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de92d2c36dbc8f0c8b02b115ed6bbe5b",
+    "url": "/Advanced_counter_Redux/index.html"
+  },
+  {
+    "revision": "8b123be543f00e050ad5",
+    "url": "/Advanced_counter_Redux/static/css/main.f2cb2d46.chunk.css"
+  },
+  {
+    "revision": "22743e35e2e514df47e9",
+    "url": "/Advanced_counter_Redux/static/js/2.c4fe755b.chunk.js"
+  },
+  {
+    "revision": "8b123be543f00e050ad5",
+    "url": "/Advanced_counter_Redux/static/js/main.714a8a80.chunk.js"
+  },
+  {
+    "revision": "9c459897b3a6c9f2ba96",
+    "url": "/Advanced_counter_Redux/static/js/runtime~main.cf3c903d.js"
+  },
+  {
+    "revision": "2eb95c771cfab09018c2785492b51f1f",
+    "url": "/Advanced_counter_Redux/static/media/1.2eb95c77.jpg"
+  }
+]);
